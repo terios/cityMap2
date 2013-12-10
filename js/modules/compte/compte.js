8dd6530cd8e7d111af70ac51c00973ebfb45a835
terios <anasyoubi@gmail.com>
2013-12-10 20:10:36 +0000

    webstorm

--- js/modules/compte/compte.js
+++ js/modules/compte/compte.js
@@ -3,8 +3,66 @@
  */
 myMapModule.controller('compteController', function ($scope) {
 
+
+    $scope.nom = 'Lieu';
+    $scope.afficheList = true;
+
+    $scope.data = [
+        {'nom': 'anas', 'ville': 'anas', 'categorie': 'oujda', 'id': 12},
+        {'nom': 'oko', 'ville': 'uu', 'categorie': 'casablanca', 'id': 13}
+    ]
+
     $scope.init = function () {
-        $scope.birthday = new Date();
+        $("#grid").kendoGrid({
+            dataSource: {
+                data: $scope.data,
+                pageSize: 10
+            },
+            groupable: true,
+            sortable: true,
+            pageable: {
+                refresh: true,
+                pageSizes: true,
+                buttonCount: 5
+            },
+            columns: [
+                {
+                    field: "nom",
+                    title: "Nom",
+                    width: 250
+                },
+                {
+                    field: "ville",
+                    title: "Ville",
+                    width: 190
+                },
+                {
+                    field: "categorie",
+                    title: "Categorie"
+                },
+                { command: [
+                    {
+                        name: "details",
+                        click: function (e) {
+                            e.preventDefault();
+                            // e.target is the DOM element representing the button
+                            var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+                            console.log(dataItem);
+                        }
+                    },
+                    {
+                        name: "suprime",
+                        click: function (e) {
+                            e.preventDefault();
+                            // e.target is the DOM element representing the button
+                            var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+                            console.log(dataItem);
+                        }
+                    }
+                ], width: 200
+                }
+            ]
+        });
     }
 
 
