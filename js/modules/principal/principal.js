ae031c18f54ee89809ab397b2ff4b9f21c81eae6
terios <anasyoubi@gmail.com>
2013-12-30 13:25:03 +0000

    webstorm

--- js/modules/principal/principal.js
+++ js/modules/principal/principal.js
@@ -17,21 +17,6 @@ myMapModule.controller('principalController', function ($rootScope, $scope, loca
      * une fois la page principal ouverte execution de la function init()
      */
     $scope.init = function () {
-        /*    $.ajax({ // fonction permettant de faire de l'ajax
-         type: "GET", // methode de transmission des données au fichier php
-         url: "http://192.168.68.102:8080/api/v1/article/search/?format=json&dist=5&lon=-3.8647105&lat=35.1713733", // url du fichier php
-         dataType: 'json',
-         success: function (data) { // si l'appel a bien fonctionnés
-
-         showPosition(data.objects);
-         $scope.$apply();
-         },
-         error: function () {
-         alert('erreur rencontrer');
-         }
-         });
-         showPosition("");
-         */
         $scope.loadmap = getlocation();
         $scope.afficheloader = true;
     }
@@ -102,45 +87,89 @@ myMapModule.controller('principalController', function ($rootScope, $scope, loca
         $scope.latitude = localisation['mapcenter'].nb;
         $scope.longitude = localisation['mapcenter'].ob;
         $rootScope.donneMarker = [
-            {nom: 'nom1', description: 'description1'},
-            {nom: 'nom2', description: 'description2'}
+            {nom: 'Hotel atlas orient', description: 'description1', category: 'diver', icon: ''},
+            {nom: 'McDonals', description: 'description2', category: 'restaurent', icon: ''},
+            {nom: "Cafe l'exelance", description: 'description3', category: 'transport', icon: ''},
+            {nom: "Cafe l'exelance", description: 'description3', category: 'mosque', icon: ''}
+
         ]
-        for (var i = 0; i < 2; i++) {
+
+        for (var i = 0; i < $rootScope.donneMarker.length; i++) {
+
+
+            /**
+             * donne a chaque categorie son marker approprie
+             */
+            switch ($rootScope.donneMarker[i]['category']) {
+                case 'hotel':
+                    $rootScope.donneMarker[i]['icon'] = "style/img/img%20icon/marker_hotel.png";
+                    break;
+                case 'restaurent':
+                    $rootScope.donneMarker[i]['icon'] = "style/img/img%20icon/marker_restaurent.png";
+                    break;
+                case 'coffe':
+                    $rootScope.donneMarker[i]['icon'] = "style/img/img%20icon/marker_cafe.png";
+                    break;
+                case 'hopital':
+                    $rootScope.donneMarker[i]['icon'] = "style/img/img%20icon/marker_hopital.png";
+                    break;
+                case 'shopping':
+                    $rootScope.donneMarker[i]['icon'] = "style/img/img%20icon/marker_shopping.png";
+                    break;
+                case 'diver':
+                    $rootScope.donneMarker[i]['icon'] = "style/img/img%20icon/marker_diver.png";
+                    break;
+                case 'mosque':
+                    $rootScope.donneMarker[i]['icon'] = "style/img/img%20icon/marker_mosque.png";
+                    break;
+                case 'transport':
+                    $rootScope.donneMarker[i]['icon'] = "style/img/img%20icon/marker_transport.png";
+                    break;
+                case 'contribution':
+                    $rootScope.donneMarker[i]['icon'] = "style/img/img%20icon/marker_contribution.png";
+                    break;
+            }
             if (i > 0) {
                 $scope.latitude = 34.677829069312956;
                 $scope.longitude = -1.9403254240751266;
             }
+            if (i > 1) {
+                $scope.latitude = 34.627829069312956;
+                $scope.longitude = -1.9403254240751266;
+            }
+            if (i > 2) {
+                $scope.latitude = 34.637829069312956;
+                $scope.longitude = -1.9403254240751266;
+            }
+            console.debug($rootScope.donneMarker[i]['icon']);
             marker = new google.maps.Marker({
                 position: new google.maps.LatLng($scope.latitude, $scope.longitude),
                 map: localisation['map'],
                 draggable: false,
                 animation: google.maps.Animation.DROP,
-                icon: 'style/img/img%20icon/hopital_marker.png',
-                category: "hotel"
+                icon: $rootScope.donneMarker[i]['icon'],
+                category: $rootScope.donneMarker[i]['category']
             });
             $rootScope.markerlist.push(marker);
 
             var boxText = document.createElement("div");
             boxText.innerHTML = '<div class="animated fadeIn">' +
-                                    '<div class="span3" style="border: 2px solid #0480be; margin-top: 0px; background:#333; color:#FFF; font-family:Arial; font-size:12px; border-radius:6px; -webkit-border-radius:6px; -moz-border-radius:6px;">' +
-                                        '<div class="row">' +
-                                                '<div class="row-fluid">'+
-                                                    '<div class="span1" style="margin-left: 40px;margin-top: 5px">' +
-                                                        '<img src="style/img/img%20icon/hotel_bg_32.png" alt="">'+
-                                                    '</div>'+
-                                                    '<div class="span2"><h4 style="color: #ffffff;margin-left: 15px">' + $rootScope.donneMarker[i]['nom'] +
-                                                    '</h4></div>' +
-                                                '</div>' +
-                                            '<div class="span3">' +
-                                                '<div class="row-fluid" style="margin-top: 5px;margin-bottom: 5px;margin-left: 10px;">' +
-                                                    '<span class=" badge badge-info">vote : 20</span>' +
-                                                    '<span class=" badge badge-primary">note : 4.2</span>' +
-                                                    '<span class="badge badge-success"><a href="#/compte" style="color: #ffffff;">plus de detaille</a></span>' +
-                                                '</div>' +
-                                            '</div>' +
-                                        '</div>' +
-                                    '</div>' +
-                                '</div>';
+                '<div class="span3" style="border: 2px solid #0480be; margin-top: 0px; background:#333; color:#FFF; font-family:Arial; font-size:12px; border-radius:6px; -webkit-border-radius:6px; -moz-border-radius:6px;">' +
+                '<div class="row">' +
+                '<div class="row-fluid">' +
+                '<div class="span8 offset2"><h4 style="color: #ffffff;margin-left: 15px">' + $rootScope.donneMarker[i]['nom'] +
+                '</h4></div>' +
+                '</div>' +
+                '<div class="span3">' +
+                '<div class="row-fluid" style="margin-top: 5px;margin-bottom: 5px;margin-left: 10px;">' +
+                '<span style="margin-right: 5px" class=" badge badge-info">vote : 20</span>' +
+                '<span style="margin-right: 5px" class=" badge badge-primary">note : 4.2</span>' +
+                '<span style="margin-right: 5px" class="badge badge-success"><a href="#/compte" style="color: #ffffff;">plus de detaille</a></span>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>';
 
             var myOptions = {
                 content: boxText,
@@ -218,7 +247,6 @@ myMapModule.controller('principalController', function ($rootScope, $scope, loca
     function ajoutPing(json) {
         i = 0;
 
-        var infowindow = new google.maps.InfoWindow();
         for (tmp in json['objects']) {
             marker = new google.maps.Marker({
                 position: new google.maps.LatLng(json['objects'][i]['latitude'], json['objects'][i]['longitude']),
@@ -228,37 +256,55 @@ myMapModule.controller('principalController', function ($rootScope, $scope, loca
                 icon: 'style/img/img%20icon/Map-Marker-coffe.png',
                 category: "hotel"
             });
+            $rootScope.markerlist.push(marker);
+
+            var boxText = document.createElement("div");
+            boxText.innerHTML = '<div class="animated fadeIn">' +
+                '<div class="span3" style="border: 2px solid #0480be; margin-top: 0px; background:#333; color:#FFF; font-family:Arial; font-size:12px; border-radius:6px; -webkit-border-radius:6px; -moz-border-radius:6px;">' +
+                '<div class="row">' +
+                '<div class="row-fluid">' +
+                '<div class="span1" style="margin-left: 40px;margin-top: 5px">' +
+                '<img src="style/img/img%20icon/hotel_bg_32.png" alt="">' +
+                '</div>' +
+                '<div class="span2"><h4 style="color: #ffffff;margin-left: 15px">' + json['objects'][i]['name'] +
+                '</h4></div>' +
+                '</div>' +
+                '<div class="span3">' +
+                '<div class="row-fluid" style="margin-top: 5px;margin-bottom: 5px;margin-left: 10px;">' +
+                '<span class=" badge badge-info">vote : ' + json['objects'][i]['votes_count'] + '</span>' +
+                '<span class=" badge badge-primary">note : ' + json['objects'][i]['rating'] + '</span>' +
+                '<span class="badge badge-success"><a href="#/compte" style="color: #ffffff;">plus de detaille</a></span>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>';
+
+            var myOptions = {
+                content: boxText,
+                disableAutoPan: false,
+                maxWidth: 0,
+                pixelOffset: new google.maps.Size(-140, 0),
+                zIndex: null,
+                boxStyle: {
+                    background: "url('http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/examples/tipbox.gif') no-repeat",
+                    opacity: 0.75,
+                    width: "280px"
+                },
+                closeBoxMargin: "-22px 0px 0px 0px",
+                closeBoxURL: "style/img/img%20icon/close_256.png",
+                infoBoxClearance: new google.maps.Size(1, 1),
+                isHidden: false,
+                pane: "floatPane",
+                enableEventPropagation: false};
+
+
+            //Define the infobox
+            $rootScope.markerlist[i].infobox = new InfoBox(myOptions);
 
             google.maps.event.addListener(marker, 'click', (function (marker, i) {
                 return function () {
-                    infowindow.setContent('<div class="animated fadeIn" style="width: 100%;background-color: darkgray">' +
-                        '<div class="row">' +
-                        '<div class="span4">' +
-                        '<div class="row">' +
-                        '<div class="span1">' +
-                        '<a class="thumbnail">' +
-                        '<img src="style/img/img%20icon/hotel_bg_32.png" alt=""></a>' +
-                        '</div>' +
-                        '<div class="span3">' +
-                        '<h4>' + json['objects'][i]['name'] + '</h4>' +
-                        '<p>' +
-                        '<strong>' + json['objects'][i]['category'] + '</strong>' +
-                        '</p>' +
-                        '<div class="row-fluid">' +
-                        '<span class=" badge badge-info">vote :' + json['objects'][i]['votes_count'] + '</span>' +
-                        '<span class=" badge badge-inverse">note :' + json['objects'][i]['rating'] + '</span>' +
-                        '<span class="badge badge-success" style="margin-left: 10px;"><a href="#" style="color: #ffffff;">plus de detaille</a></span>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>'
-                    )
-                    ;
-                    infowindow.open(localisation['map'], marker);
-                    //localisation['map'].setZoom(8);
-                    //localisation['map'].setCenter(marker.getPosition());
+                    $rootScope.markerlist[i].infobox.open(localisation['map'], this);
                 }
             })(marker, i));
             $rootScope.markerlist.push(marker);
